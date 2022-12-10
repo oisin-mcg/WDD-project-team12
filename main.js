22e9c132d9188781610d24ee4e2c3f70756c9236
TadamP <116078864+TadamP@users.noreply.github.com>
2022-12-10 20:03:04 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -5,23 +5,19 @@
                 var msge = "You have not fully completed the form, ";
                 //use if statements to check the data and set the message 
                 if(document.getElementById("fn").value == ""){
-                    msge += "You need to fill in your Firstname. ";
+                    msge += "Firstname ";
                     valid = false;
                 }
                 if(document.getElementById("sn").value == ""){
-                    msge += "You need to fill in your Surname. ";
+                    msge += "Surname. ";
                     valid = false;
                 }
                 if(document.getElementById("cn").value == ""){
-                    msge += "You need to fill in your age. ";
+                    msge += "age. ";
                     valid = false;
                 }
 				if(document.getElementById("bn").value == ""){
-                    msge += "You need to fill in your email, aswell as your title. ";
-                    valid = false;
-                }
-				if(document.getElementById("rn").value == ""){
-                    msge += "You need to fill in your email. ";
+                    msge += " email, aswell as your title. ";
                     valid = false;
                 }
                 if(!valid){ //!valid is the same as valid == false
