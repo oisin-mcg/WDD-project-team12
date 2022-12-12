7ce58b6654137f22cdca6ee3729a6f6a6835908f
TadamP <116078864+TadamP@users.noreply.github.com>
2022-12-12 23:54:26 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -2,22 +2,26 @@
  function validate(){
                 //set valid to true - flag 
                 var valid = true;
-                var msge = "You have not fully completed the form, ";
+                var msge = "You have not fully completed the form, please fill in the following: ";
                 //use if statements to check the data and set the message 
                 if(document.getElementById("fn").value == ""){
-                    msge += "You need to fill in your Firstname. ";
+                    msge += " You must fill in your firstname. ";
                     valid = false;
                 }
                 if(document.getElementById("sn").value == ""){
-                    msge += "You need to fill in your Surname. ";
+                    msge += " You must fill in your surname, ";
                     valid = false;
                 }
                 if(document.getElementById("cn").value == ""){
-                    msge += "You need to fill in your age. ";
+                    msge += " You must fill in your age, ";
                     valid = false;
                 }
 				if(document.getElementById("bn").value == ""){
-                    msge += "You need to fill in your email.";
+                    msge += " You must fill in your email,";
+                    valid = false;
+                }
+				if(document.getElementById("comments").value == ""){
+                    msge += " You need to write your question in the comment box.";
                     valid = false;
                 }
                 if(!valid){ //!valid is the same as valid == false
